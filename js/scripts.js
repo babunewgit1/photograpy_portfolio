809d64db378c9650cdf61c1ddac9be2b8b08732e
babunewgit1 <babunewx11@gmail.com>
2024-06-06 22:55:13 +0600

    done

--- js/scripts.js
+++ js/scripts.js
@@ -46,10 +46,10 @@
 
     var owl = $(".products-slider");
     owl.owlCarousel();
-    $(".right").click(function () {
+    $(".controlar1 .right").click(function () {
       owl.trigger("next.owl.carousel");
     });
-    $(".left").click(function () {
+    $(".controlar1 .left").click(function () {
       owl.trigger("prev.owl.carousel", [300]);
     });
 
@@ -84,11 +84,87 @@
 
     var owl2 = $(".products-slider2");
     owl2.owlCarousel();
-    $(".right2").click(function () {
+    $(".controlar2 .right2").click(function () {
       owl2.trigger("next.owl.carousel");
     });
-    $(".left2").click(function () {
+    $(".controlar2 .left2").click(function () {
       owl2.trigger("prev.owl.carousel", [300]);
+    });
+
+    $(".products-slider3").owlCarousel({
+      loop: true,
+      margin: 0,
+      center: true,
+      nav: false,
+      dots: true,
+      autoplay: false,
+      autoplayTimeout: 2000,
+      responsiveClass: true,
+      responsiveRefreshRate: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 3,
+        },
+        960: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+        },
+        1920: {
+          items: 3,
+        },
+      },
+    });
+
+    var owlx = $(".products-slider3");
+    owlx.owlCarousel();
+    $(".controlar3 .right").click(function () {
+      owlx.trigger("next.owl.carousel");
+    });
+    $(".controlar3 .left").click(function () {
+      owlx.trigger("prev.owl.carousel", [300]);
+    });
+
+    $(".products-sliderz").owlCarousel({
+      loop: true,
+      margin: 0,
+      center: true,
+      nav: false,
+      dots: true,
+      autoplay: false,
+      autoplayTimeout: 2000,
+      responsiveClass: true,
+      responsiveRefreshRate: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 3,
+        },
+        960: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+        },
+        1920: {
+          items: 3,
+        },
+      },
+    });
+
+    var owlz = $(".products-sliderz");
+    owlz.owlCarousel();
+    $(".controlarz .right2").click(function () {
+      owlz.trigger("next.owl.carousel");
+    });
+    $(".controlarz .left2").click(function () {
+      owlz.trigger("prev.owl.carousel", [300]);
     });
 
     $(".ps-slider").owlCarousel({
